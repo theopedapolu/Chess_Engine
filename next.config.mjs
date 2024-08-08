dcc7ae57ad37f063f03dee716caefe4b01aa0a12
theopedapolu <theopedapolu@Theophiluss-MBP.lan>
2024-08-08 02:18:54 -0400

    Updated config for github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.output.globalObject = 'self';
     }
     return config;
+  },
+  basePath: '/Chess_Engine',
+  images: {
+    unoptimized: true,
   },
 };
 
